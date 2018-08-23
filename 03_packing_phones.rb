726743d84b30e9670c3accc45970feca95f06d3a
Flynn <flynn@Flynns-MacBook-Pro.local>
2018-08-23 12:11:39 +1000

    03_packing_phones.rb

---
+++ 03_packing_phones.rb
@@ -0,0 +1,3 @@
+# 1. sit beside someone different in a different spot from yesterday
+
+# 2. A business called “The Company” created a new phone called the companyPhone that needs packing to send away for sale. The Company needs to know how many phones in their pretty new packaging (rectangular prisms) that measures: with  width 5cm, length 7.4cm, and depth 4cm we can fit into a box that is 32cm wide,  43cm long and 22.1cm high.   (think about writing some pseudo code first that steps through all the commands you need to write. Keep it as simple as you can - this is not a ‘bin packing’ problem)
