2234a1ea624c7c2091adaa3349045e8a971711c5
Flynn <flynnwebdev@gmail.com>
2018-08-27 22:32:34 +1000

    Fixed update.sh

--- update.sh
+++ update.sh
@@ -3,6 +3,6 @@ git add .
 git stash
 git fetch https://github.com/CoderAcademy-BRI/morning-challenges-term1.git
 git checkout FETCH_HEAD -- $1*
-git stash pop
+git checkout stash -- .
 echo "Done!"
 ls
