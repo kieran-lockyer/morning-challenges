25831022c6e80a84f261894d42fef60266ea232b
Flynn <matt.etherington@coderacademy.edu.au>
2018-08-28 13:26:59 +1000

    Initial commit - 04_fib_seq

---
+++ 04_fib_seq.rb
@@ -0,0 +1,3 @@
+# 1. sit beside someone new, at a new spot :slightly_smiling_face:
+
+# 2. There is an odd employee at The Company who needs some code written that will provide them with the first 100 numbers of the Fibonacci sequence ….. (no one really knows why the odd employee needs this information - or why they can’t work it out themselves - but Monday mornings can be strange).
