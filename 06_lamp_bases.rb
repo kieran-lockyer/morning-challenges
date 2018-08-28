2dd3bf6d350f51f2b148f73a8054f58a77bc780b
Flynn <matt.etherington@coderacademy.edu.au>
2018-08-28 13:27:20 +1000

    Initial commit - 06_lamp_bases

---
+++ 06_lamp_bases.rb
@@ -0,0 +1,4 @@
+# The Company is currently wanting to produce another 500 units of product sku 276834, which is a lamp with a circular base. Unfortunately, a staff member forgot to order the plywood to make the base and didn’t account for the cost of this wood.
+# Plywood can be purchased in 1200 X 2400 mm sheets at a cost of $28.47 per sheet.
+# Each lamp base is a circle with a diameter of 320mm. 
+# What is the total cost of ordering the plywood for the bases, and how much plywood is wasted?
