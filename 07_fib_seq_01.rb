9f856314340875f6ad7ac97632b0b4ea314bc9c6
Flynn <matt.etherington@coderacademy.edu.au>
2018-09-03 10:15:48 +1000

    sync master

--- 07_fib_seq_01.rb
+++ 07_fib_seq_01.rb
@@ -2,11 +2,13 @@
 
 # The Company has broken the keyboard on their one and only laptop, so can’t get this bit of code to run. What would the output of this code be (without running it on your own computer)? 
 
-#  def fib(num_
-#   if num < 2
-#      num
-#   else
-#      fib(num-1) + fib(num-2)
-#   end
-# end
-# puts fib(4)
+ def fib(num_
+  if num < 2
+     num
+  else
+     fib(num-1) + fib(num-2)
+  end
+end
+puts fib(4)
+
+# It would output a syntax error due to the underscore instead of a close parenthesis on line 5.
