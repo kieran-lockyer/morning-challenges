96e1d226f4fe9a905e5ac7002ae08881529c8128
kieran-lockyer <kieran.lockyer.87@gmail.com>
2018-09-26 09:57:07 +1000

    morning challenge completed

--- 22_card_suit.rb
+++ 22_card_suit.rb
@@ -8,6 +8,16 @@
 # ('3S') -> return 'spades'
 
 def define_suit(card)
-  # Good luck
+  suit = card[1]
+  case suit
+  when "C"
+    return 'clubs'
+  when "D"
+    return 'diamonds'
+  when "H"
+    return 'hearts'
+  when "S"
+    return 'spades'
+  end
 end
 
