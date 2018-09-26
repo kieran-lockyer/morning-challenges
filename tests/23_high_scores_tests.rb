ef19f1be7c75962fb9062c4b9e2ea4fc7cfef39c
Flynn <matt.etherington@coderacademy.edu.au>
2018-09-27 09:41:07 +1000

    23_high_scores

--- tests/23_high_scores_tests.rb
+++ tests/23_high_scores_tests.rb
@@ -7,33 +7,33 @@ class CardSuitTest < Test::Unit::TestCase
     @@high_scores = HighScoreTable.new(3)
 
     def test_empty
-        assert_equal(@@high_scores.scores, [])
+        assert_equal([], @@high_scores.scores)
     end
     def test_add_one_score
         @@high_scores.update(10)
-        assert_equal(@@high_scores.scores, [10])
+        assert_equal([10], @@high_scores.scores)
     end
     def test_add_multiple_scores
         @@high_scores.update(8)
         @@high_scores.update(12)
-        assert_equal(@@high_scores.scores, [12, 10, 8])
+        assert_equal([12, 10, 8], @@high_scores.scores)
         @@high_scores.update(5)
-        assert_equal(@@high_scores.scores, [12, 10, 8])
+        assert_equal([12, 10, 8], @@high_scores.scores)
     end
     def test_add_duplicate_scores
         @@high_scores.update(10)
-        assert_equal(@@high_scores.scores, [12, 10, 10])
+        assert_equal([12, 10, 10], @@high_scores.scores)
         @@high_scores.update(10)
-        assert_equal(@@high_scores.scores, [12, 10, 10])
+        assert_equal([12, 10, 10], @@high_scores.scores)
         @@high_scores.update(20)
-        assert_equal(@@high_scores.scores, [20, 12, 10])
+        assert_equal([20, 12, 10], @@high_scores.scores)
         @@high_scores.update(20)
-        assert_equal(@@high_scores.scores, [20, 20, 12])
+        assert_equal([20, 20, 12], @@high_scores.scores)
         @@high_scores.update(20)
-        assert_equal(@@high_scores.scores, [20, 20, 20])
+        assert_equal([20, 20, 20], @@high_scores.scores)
     end
     def test_reset_scores
         @@high_scores.reset()
-        assert_equal(@@high_scores.scores, [])
+        assert_equal([], @@high_scores.scores)
     end
 end
