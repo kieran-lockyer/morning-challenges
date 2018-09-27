af349a38d6b3584a2405f576a29279ed5a635e47
Flynn <matt.etherington@coderacademy.edu.au>
2018-09-28 09:16:18 +1000

    24_card_game

---
+++ 24_card_game.rb
@@ -0,0 +1,13 @@
+class Card
+    def initialize(suit, rank)
+        # Create a new card
+    end
+    
+    def face_card?
+        # Is the card a face card (above 10)?
+    end
+    
+    def to_s
+        # Return human readable card
+    end
+end
