61b0d7103bfce16e40155a8424f497b458a8258d
kieran-lockyer <kieran.lockyer.87@gmail.com>
2018-10-04 11:16:42 +1000

    sync master

--- 27_maze.rb
+++ 27_maze.rb
@@ -1,11 +1,99 @@
 class Maze
     def initialize(maze)
-        
+        @maze = maze
+        for row in maze
+            if row.include?(2)
+                @start_row = @maze.index(row)
+                @start_column = @maze[@maze.index(row)].index(2)
+            elsif row.include?(3)
+                @end_row = @maze.index(row)
+                @end_column = @maze[@maze.index(row)].index(3)
+            end
+        end
+        @start = [@start_row, @start_column]
+        @end = [@end_row, @end_column]
+        @status = "Lost"
+        @position = [@start_row, @start_column]
     end
-    def walk(moves)
-        
-    end
-    def can_move?(direction)
 
+    def walk(moves)
+        if @status != "Lost"
+            @position = [@start_row, @start_column]
+        end
+        puts "Starting stats"
+        puts "start #{@start.inspect}"
+        puts "end #{@end.inspect}"
+        puts "status #{@status}"
+        puts "position #{@position.inspect}"
+        puts "position == start: #{@position == @start}"
+        puts "---------------------------------------------------------"
+        for move in moves
+            can_move?(move)
+            case @maze[@position[0]][@position[1]]
+            when 3
+                if @status != "Dead"
+                    @status = "Finish"
+                end
+            when 1
+                @status = "Dead"
+            end
+        end
+        puts "Ending stats"
+        puts "start #{@start.inspect}"
+        puts "end #{@end.inspect}"
+        puts "status #{@status}"
+        puts "position #{@position.inspect}"
+        puts "position == start: #{@position == @start}"
+        puts "---------------------------------------------------------"
+        return @status
+    end
+
+    def can_move?(direction)
+        case direction
+        when "N"
+            test_ground = [@position[0] - 1, @position[1]]
+            for ground in test_ground
+                if ground < @maze.length or ground > 0
+                    @position[0] = @position[0] - 1
+                    return true
+                else
+                    @status = "Dead"
+                    return false
+                end
+            end
+        when "S"
+            test_ground = [@position[0] + 1, @position[1]]
+            for ground in test_ground
+                if ground < @maze.length or ground > 0
+                    @position[0] = @position[0] + 1
+                    return true
+                else
+                    @status = "Dead"
+                    return false
+                end
+            end
+        when "E"
+            test_ground = [@position[0], @position[1] + 1]
+            for ground in test_ground
+                if ground < @maze.length or ground > 0
+                    @position[1] = @position[1] + 1
+                    return true
+                else
+                    @status = "Dead"
+                    return false
+                end
+            end
+        when "W"
+            test_ground = [@position[0], @position[1] - 1]
+            for ground in test_ground
+                if ground < @maze.length or ground > 0
+                    @position[1] = @position[1] - 1
+                    return true
+                else
+                    @status = "Dead"
+                    return false
+                end
+            end
+        end
     end
 end
--- tests/27_maze_test.rb
+++ tests/27_maze_test.rb
@@ -54,36 +54,43 @@ class TestMaze < Test::Unit::TestCase
     end
     # If we reach number 3, we've finished the maze
     def test_finish
+        puts "Finish Test"
         assert_equal("Finish", @maze.walk(["N","N","N","N","N","E","E","E","E","E"]))
         assert_equal("Finish", @maze.walk(["N","N","N","N","N","E","E","S","S","E","E","N","N","E"]))
         assert_equal("Finish", @maze.walk(["N","N","N","N","N","E","E","E","E","E","W","W"]))
     end
     # Test for finish should ignore extra moves
     def test_finish_on_tenth_move
+        puts "Finish Test"
         # Will return "Dead" if it doesn't ignore the 11th move, as we'll fall off the edge
         assert_equal("Finish", @maze.walk(["N","N","N","N","N","E","E","E","E","E","E"]))
     end
     # Should be possible to finish without doing it all in a single walk
     def test_finish_multiple_walks
+        puts "Multiple walks Test"
         assert_equal("Lost", @maze.walk(["N","N","N","N","N"]))
         assert_equal("Finish", @maze.walk(["E","E","E","E","E"]))
     end
     # If we go off the edge of the maze, we're dead!
     def test_dead
+        puts "Dead Test"
         assert_equal("Dead", @maze.walk(["N","N","N","W","W"]))
         assert_equal("Dead", @maze.walk(["N","N","N","N","N","E","E","S","S","S","S","S","S"]))
     end
     # Remaining moves should be ignored if we fell off the maze before the end of the walk
     def test_dead_on_fifth_move
+        puts "Dead Test"
         assert_equal("Dead", @maze.walk(["N","N","N","W","W","E","E","N"]))
     end
     # If we didn't finish or die by the end of the walk, we're lost in the maze!
     def test_lost
+        puts "Lost Test"
         assert_equal("Lost", @maze.walk(["N","E","E","E","E"]))
         assert_equal("Lost", @maze.walk(["N","E","E","N"]))
     end
     # Walls carry high voltage, so if we touch one, we're dead!
     def test_hit_wall
+        puts "Hit Wall Test"
         assert_equal("Dead", @maze.walk(["N","W","W","E","E"]))
         assert_equal("Dead", @maze.walk(["N","N","N","N","N","E","E","E","E","N","N","E"]))
     end
