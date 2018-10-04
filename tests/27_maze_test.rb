e3f9f4e73af2083c93337490ff8271225687f885
Flynn <matt.etherington@coderacademy.edu.au>
2018-10-04 10:55:59 +1000

    Fixed 27_maze_test

--- tests/27_maze_test.rb
+++ tests/27_maze_test.rb
@@ -53,9 +53,13 @@ class TestMaze < Test::Unit::TestCase
         ])    
     end
     # If we reach number 3, we've finished the maze
-    def test_finish
+    def test_finish_route_1
         assert_equal("Finish", @maze.walk(["N","N","N","N","N","E","E","E","E","E"]))
+    end
+    def test_finish_route_2
         assert_equal("Finish", @maze.walk(["N","N","N","N","N","E","E","S","S","E","E","N","N","E"]))
+    end
+    def test_finish_route_3
         assert_equal("Finish", @maze.walk(["N","N","N","N","N","E","E","E","E","E","W","W"]))
     end
     # Test for finish should ignore extra moves
@@ -69,8 +73,10 @@ class TestMaze < Test::Unit::TestCase
         assert_equal("Finish", @maze.walk(["E","E","E","E","E"]))
     end
     # If we go off the edge of the maze, we're dead!
-    def test_dead
+    def test_dead_1
         assert_equal("Dead", @maze.walk(["N","N","N","W","W"]))
+    end
+    def test_dead_2
         assert_equal("Dead", @maze.walk(["N","N","N","N","N","E","E","S","S","S","S","S","S"]))
     end
     # Remaining moves should be ignored if we fell off the maze before the end of the walk
@@ -78,13 +84,17 @@ class TestMaze < Test::Unit::TestCase
         assert_equal("Dead", @maze.walk(["N","N","N","W","W","E","E","N"]))
     end
     # If we didn't finish or die by the end of the walk, we're lost in the maze!
-    def test_lost
+    def test_lost_1
         assert_equal("Lost", @maze.walk(["N","E","E","E","E"]))
+    end
+    def test_lost_2
         assert_equal("Lost", @maze.walk(["N","E","E","N"]))
     end
     # Walls carry high voltage, so if we touch one, we're dead!
-    def test_hit_wall
+    def test_hit_wall_1
         assert_equal("Dead", @maze.walk(["N","W","W","E","E"]))
+    end
+    def test_hit_wall_2
         assert_equal("Dead", @maze.walk(["N","N","N","N","N","E","E","E","E","N","N","E"]))
     end
 end
