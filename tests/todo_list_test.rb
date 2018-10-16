16046f2ef987cb3ae10db3c12306d00c5abb3080
caadmin <janel.brandon@coderacademy.edu.au>
2018-10-17 08:50:57 +1000

    Revert "Remove erroneous files from master"
    This reverts commit 904a3dc53c590e815f587ed4cb9381379de7b81c.

---
+++ tests/todo_list_test.rb
@@ -0,0 +1,50 @@
+require "test/unit"
+require_relative "../todo_list"
+
+class TodoListTest < Test::Unit::TestCase
+    def setup
+        @todo_list = TodoList.new("My List")
+        item = @todo_list.add_item("Get cat food")
+    end
+
+    # Can create a new todo list
+    def test_create_todo_list
+        assert_equal(1,@todo_list.count)
+        assert_equal("My List",@todo_list.title)
+    end
+
+    # Can create a todo item in an existing list
+    def test_create_todo_item
+        item = @todo_list.add_item("Update resume")
+        assert_equal(2, @todo_list.count)
+    end
+
+    # Can update the title of a todo list
+    def test_update_todo_list_title
+        @todo_list.title = "My Updated List"
+        assert_equal("My Updated List", @todo_list.title)
+    end
+
+    # Can update the description of a todo list
+    def test_update_todo_list_description
+        @todo_list.description = "Things I want to do this week"
+        assert_equal("Things I want to do this week", @todo_list.description)
+    end
+
+    # Can check if all items are done on list
+    def test_get_items_all_done
+        assert_equal(false, @todo_list.all_done?)
+    end
+
+    # Can delete an item from a list
+    def test_delete_todo_item
+        @todo_list.delete_item("Get cat food")
+        assert_equal(0,@todo_list.count)
+    end
+
+    # Can update an item to done on the list
+    def test_update_todo_item_done
+        @todo_list.update_done("Get cat food")
+        assert_equal(true, @todo_list.get_item("Get cat food").done?)
+    end
+end
---
+++ todo_list.rb
@@ -0,0 +1,18 @@
+# For this challenge, your Todo List should be able to perform all actions
+# given in the test. Namely you should be able to:
+# 1. create a new todo list with a title and description
+# 2. update the title or description
+# 3. get a count of items in the list
+# 4. check to see if all items in the list are marked done
+# 5. mark all items in the list as done
+# 6. add items to the list
+# 7. mark a single item in the list as done
+# 8. delete items from the list
+
+# To accomplish this, you should define two classes - a TodoList class
+# and a TodoListItem class.
+class TodoList
+end
+
+class TodoListItem
+end
