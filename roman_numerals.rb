7b9149363268f29c42d4caeaf255b431d285013f
kieran-lockyer <kieran.lockyer.87@gmail.com>
2018-10-22 08:34:52 +1000

    sync master

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -23,8 +23,44 @@
 #   M	    1000
 #
 class RomanNumerals
+    @values = {
+        "M": 1000,
+        "CM": 900,
+        "D": 500,
+        "CD": 400,
+        "C": 100,
+        "XC": 90,
+        "L": 50,
+        "XL": 40,
+        "X": 10,
+        "IX": 9,
+        "V": 5,
+        "IV": 4,
+        "I": 1
+    }
     def self.fromRoman(number)
+        result = 0
+        last_num = 0
+        for numeral in number.split("").reverse
+            if @values[numeral.to_sym] >= last_num
+                result += @values[numeral.to_sym]
+                last_num = @values[numeral.to_sym]
+            else
+                result -= @values[numeral.to_sym]
+            end
+        end
+        return result
     end
     def self.toRoman(number)
+        result = ""
+        for roman, int in @values
+            if int <= number
+                while int <= number
+                    number -= int
+                    result += roman.to_s
+                end
+            end
+        end
+        return result
     end
 end
