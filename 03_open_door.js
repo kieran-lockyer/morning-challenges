8861500e4771ea6bf21c5ae0fdc797b5a9e4ba71
kieran-lockyer <kieran.lockyer.87@gmail.com>
2018-11-15 09:16:43 +1000

    sync master

--- 03_open_door.js
+++ 03_open_door.js
@@ -7,8 +7,7 @@ mocha 03_open_door.js
 */
 
 function openDoor(input) {
-    // Your code here
-
+    return input.map(x => x.name === 'Simon' && x.hasCoffee).includes(true)
 }
 
 function makeItem(name, hasCoffee) {
@@ -21,25 +20,29 @@ function makeItem(name, hasCoffee) {
 var assert = require('assert');
 
 describe('openDoor', function () {
-    let hash = [];
     it('should return true if "Simon" and hasCoffee with single object', function () {
+        let hash = [];
         hash.push(makeItem('Simon', true))
         assert.equal(true, openDoor(hash))
     })
     it('should return true if "Simon" and hasCoffee with multiple objects', function () {
+        let hash = [];
         hash.push(makeItem('Simon', true))
         hash.push(makeItem('Scott', true))
         assert.equal(true, openDoor(hash))
     })
     it('should return false if !"Simon" and hasCoffee', function () {
+        let hash = [];
         hash.push(makeItem('Scott', true))
         assert.equal(false, openDoor(hash))
     })
     it('should return false if "Simon" and !hasCoffee', function () {
+        let hash = [];
         hash.push(makeItem('Simon', false))
         assert.equal(false, openDoor(hash))
     })
     it('should return false if empty array passed', function () {
+        let hash = [];
         assert.equal(false, openDoor(hash))
     })
 })
