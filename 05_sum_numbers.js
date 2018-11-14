608620fd724f03ee49fdf0f5cf07a0a5f126ea2e
caadmin <janel.brandon@coderacademy.edu.au>
2018-11-14 16:24:19 +1000

    made it a little harder

--- 05_sum_numbers.js
+++ 05_sum_numbers.js
@@ -1,17 +1,52 @@
-// Write a method that will take an array of numbers,
-// and return their sum.
+// Write a method that will take an array player scores for a series of games
+// and return the name of the player with the highest total score.
 // Test your solution:
 // mocha 05_sum_numbers.js
 
-function sumNumber () {
-    // Your code here
+function sumScores(scores) {
+    let sum = 0
+    for (score of scores) {
+        sum += score
+    }
+    return score
 }
+
+function findWinner(players) {
+    // Your code here
+    let winner = ''
+    let highestTotal = 0
+
+    for (player of players) {
+        let totalScore = sumScores(player.scores)
+        if (totalScore > highestTotal) {
+            winner = player.name
+        }
+    }
+    return winner
+}
+
 
 var assert = require('assert');
 
-describe('sumNumber', function() {
-    it('Should return the sum number', function() {
-        assert.equal(3, sumNumber([1,1,1]));
-        assert.equal(117, sumNumber([5,2,100,0,10]));
+class Player {
+    constructor(name, scores) {
+        this.name = name
+        this.scores = scores
+    }
+}
+
+let players = []
+describe('findWinner', function () {
+    it('Should return the winner when winner is first in array', function () {
+        players = [new Player('James', [50, 100, 85], new Player('Nathan', [55, 90, 86]))]
+        assert.equal('James', findWinner(players))
+    })
+    it('Should return the winner when winner is second in array', function () {
+        players = [new Player('Nathan', [50, 100, 85], new Player('James', [55, 90, 86]))]
+        assert.equal('Nathan', findWinner(players))
+    })
+    it('Should return the first player when both players are tied', function () {
+        players = [new Player('Nathan', [50, 100, 85], new Player('James', [50, 100, 85]))]
+        assert.equal('Nathan', findWinner(players))
     })
 })
