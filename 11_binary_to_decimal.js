5da9175a624e07172a9cefadf61c842ec1f36d50
Ryan Grosser <33079422+RyanG91@users.noreply.github.com>
2018-11-22 13:53:22 +1000

    Added beast mode (#13)
    * Added beast mode
    
    * Small changes to description

--- 11_binary_to_decimal.js
+++ 11_binary_to_decimal.js
@@ -5,6 +5,11 @@ Examples:
     binaryToDecimal(1000) => 8
     binaryToDecimal(1011) => 11
     binaryToDecimal(1100) => 12
+
+Your solution should not use parseInt
+
+Test: mocha 11_binary_to_decimal
+
 */
 
 function binaryToDecimal(binaryNumber) {
