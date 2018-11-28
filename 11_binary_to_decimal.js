c61700a91efba40255cfe668184e4530f92fb238
kieran-lockyer <kieran.lockyer.87@gmail.com>
2018-11-29 08:54:28 +1000

    sync master

--- 11_binary_to_decimal.js
+++ 11_binary_to_decimal.js
@@ -13,19 +13,19 @@ Test: mocha 11_binary_to_decimal
 */
 
 function binaryToDecimal(binaryNumber) {
-    // Your code here
+    return !binaryNumber.toString().match(/[2-9]/) ? binaryNumber.toString().split('').reverse().map((a, b) => a * (2 ** b)).reduce((x, y) => x + y) : null
 }
 
 let assert = require('assert')
 
 describe('Binary to decimal', function () {
     it('Should convert a binary to its decimal value', function () {
-        assert.equal((9),binaryToDecimal(1001))
-        assert.equal((15),binaryToDecimal(1111))
-        assert.equal((31),binaryToDecimal(11111))
+        assert.equal((9), binaryToDecimal(1001))
+        assert.equal((15), binaryToDecimal(1111))
+        assert.equal((31), binaryToDecimal(11111))
     })
     it('Should return a null if the binary has a number other than 0 or 1', function () {
-        assert.equal((null),binaryToDecimal(1002))
-        assert.equal((null),binaryToDecimal(1220))
+        assert.equal((null), binaryToDecimal(1002))
+        assert.equal((null), binaryToDecimal(1220))
     })
 })
