7c5ece9d895486f1b63bb99d19f7727b6066548e
caadmin <janel.brandon@coderacademy.edu.au>
2018-12-09 09:33:54 +1000

    update a couple of tests

--- 20_pangram.js
+++ 20_pangram.js
@@ -12,32 +12,32 @@ If the given string has any uppercase or numbers, it should return false
 
 */
 
-function isPangram(string){
+function isPangram(string) {
     //Your code here
 }
 
 const assert = require('assert');
 
-describe('Pangram Challenge', function() {
-    it('The phrase "the quick brown fox jumps over the lazy dog" should return true', function() {
+describe('Pangram Challenge', function () {
+    it('The phrase "the quick brown fox jumps over the lazy dog" should return true', function () {
         assert.equal(isPangram('the quick brown fox jumps over the lazy dog'), true)
     })
-    it('The phrase "the five boxing kanye west wizards jump quickly" should return true', function() {
+    it('The phrase "the five boxing kanye west wizards jump quickly" should return true', function () {
         assert.equal(isPangram('the five boxing kanye west wizards jump quickly'), true)
     })
-    it('The phrase "take the dog for a walk" should return false', function() {
+    it('The phrase "take the dog for a walk" should return false', function () {
         assert.equal(isPangram('take the dog for a walk'), false)
     })
-    it('The phrase "alright alright alright" should return false', function() {
+    it('The phrase "alright alright alright" should return false', function () {
         assert.equal(isPangram('alright alright alright'), false)
     })
     it('Should return false if numbers are included', function () {
         assert.equal(isPangram('abcdefghijk1mn0pqrstuvwxyz'), false)
     })
-    it('Should return false if only numbers are included', function() {
-        assert.equal(isPangram(1,2,3,4), false)
+    it('Should return false if only numbers are included', function () {
+        assert.equal(isPangram('1,2,3,4'), false)
     })
-    it('Should return false if any letters are uppercase', function() {
-        assert.equal(isPangram('ABCDEFGHIJKLMNOPQRSTUVWXYZ'), false)
+    it('Should return true for uppercase', function () {
+        assert.equal(isPangram('ABCDEFGHIJKLMNOPQRSTUVWXYZ'), true)
     })
 })
