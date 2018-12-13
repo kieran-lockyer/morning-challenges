43f0851bb82a96d29b522f9cc5c2691d363c254d
RyanG91 <ryan.grosser91@gmail.com>
2018-12-13 15:42:02 +1000

    Changes to the tests

--- 22_uniqueString.js
+++ 22_uniqueString.js
@@ -17,10 +17,7 @@ const assert = require('assert')
 describe('Unique string challenge', function() {
     it('Should return the unique string', function() {
         assert.deepEqual(uniqueString(['aa', 'AaA', 'aaaa', 'bBbB', 'aAaA', 'a']), 'bBbB')
-        assert.deepEqual(uniqueString(['kios', 'foo bar', 'ikos', 'ikso', 'kois', 'kiso']), 'foo bar')
-    })
-    it('Return "A number is not a string" if there are numbers present', function() {
-        assert.deepEqual(uniqueString(['sss', 'sssss', 22, 'SSSS']), "A number is not a string")
-        assert.deepEqual(uniqueString([2, 222, 'kkkkkk', 6.543]), "A number is not a string")
+        assert.deepEqual(uniqueString(['kios', 'foo', 'ikos', 'ikso', 'kois', 'kiso']), 'foo')
+        assert.deepEqual(uniqueString(['abc', ' ', '  ']), 'abc')
     })
 })
