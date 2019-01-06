3539eedf17212951a27361d27e4a344d4b2f5ebc
Ryan Grosser <33079422+RyanG91@users.noreply.github.com>
2019-01-07 06:47:28 +1000

    Equal values challenge added (#16)

---
+++ 28_equalValues.js
@@ -0,0 +1,42 @@
+/*
+Use the equalValues function to return true if
+two arguments hold exactly the same values and 
+false if they do not.
+The supplied arguments will be objects, arrays and strings.
+
+Examples:
+
+{kanye:7, west:2} & {kanye:7, west:2} => true 
+{adam:3, laura:4, maisie:2} & {laura:4, adam:3} => false
+[{a:4},{b:3}] & [{a:4},{b:3}]) => true
+{a:['5','6','7'], b:['4','5','6']} & {a:[5,6,7], b:[4,5,6]} => false
+'Alright' & 'Alright' => true
+*/
+
+const equalValues = (value1, value2) => {
+    // Your code below
+}
+
+const assert = require('assert')
+describe('equalValues tests', () => {
+    it('Should return true if the values are the same', () => {
+        assert.equal(equalValues({a:[2,3],b:[4]},{a:[2,3],b:[4]}), true)
+        assert.equal(equalValues({adam:3, laura:4},{adam:3, laura:4}), true)
+    })
+    it('Should return false if the values are not the same', () => {
+        assert.equal(equalValues([{a:3},{b:4}],[{a:'3'},{b:'4'}]), false)
+        assert.equal(equalValues({adam:3, laura:4, maisie:2},{adam:3, laura:4}), false)
+        assert.equal(equalValues({a:3},{b:4},{b:3},{a:4}), false)
+        assert.equal(equalValues({a:3,b:2},{a:3},{b:2}), false)
+    })
+// -------------------------------------------------------------------
+//                         Beast Mode - uncomment lines below
+// -------------------------------------------------------------------
+    // it('Beast mode: Should return false if the values are not in the same order', () => {
+    //     assert.equal(equalValues({a:3,b:2},{b:2,a:3}), false)
+    // })
+    // it('Beast mode++: Should return true if one value is in JSON', () => {
+    //     assert.equal(equalValues({a:3,b:4},{{"a":"4"},{"b":"4"}}), true)
+    // })
+})
+
