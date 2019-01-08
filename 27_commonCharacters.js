f50c85c5a3ea9178f5488cbd93cb22cdebe2c556
Ryan Grosser <33079422+RyanG91@users.noreply.github.com>
2019-01-09 09:05:55 +1000

    Common characters challenge (#18)

---
+++ 27_commonCharacters.js
@@ -0,0 +1,46 @@
+/*
+Write code which will return the common characters found in
+two different strings. You should only need to return the
+characters once.
+
+Examples:
+commonCharacters("efg", "feg") => "efg"
+commonCharacters("We are both dragon energy", "I am God's vessel") => "eaodg"
+commonCharacters("Alright alright alright", "") => ""
+
+*/
+
+const commonCharacters = (string1, string2) => {
+    //    Your code here
+
+}
+
+// Your Code Should Pass:
+const chai = require('chai');
+const should = chai.should();
+    
+describe('Common Characters', () => {
+    it('returns commons characters', () => {
+        commonCharacters('abc', 'abc').should.equal('abc')
+    })
+
+    it('returns common characters', () => {
+        commonCharacters("What is love?", "Baby don't hurt me").should.equal('hatoe')
+    })
+
+    it('returns common characters', () => {
+        commonCharacters('Riding on a buffalo makes me more approachable', 'so what').should.equal('oash')
+    })
+
+    it('returns empty string', () => {
+        commonCharacters('', 'No more').should.equal('')
+    })
+
+    it('returns empty string', () => {
+        commonCharacters('No more', '').should.equal('')
+    })
+
+    it('returns empty string', () => {
+        commonCharacters('', '').should.equal('')
+    })
+})
