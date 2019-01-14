50ec6ef665d4db13f351acffb106ccdeffb4a51d
kieran-lockyer <kieran.lockyer.87@gmail.com>
2019-01-15 09:00:30 +1000

    sync master

--- 30_sample.js
+++ 30_sample.js
@@ -11,8 +11,20 @@ sample([1, 2, 3, 4, 5, 6], 3);
 => [1, 6, 2]
 */
 
-const sample = () => {
-//  Your code here
+const sample = (arr, n = 1) => {
+  if (n < 0) {
+    n = Math.abs(n)
+  }
+  if (n >= arr.length) {
+    return arr
+  }
+  result = []
+  for (let x = 0; x < n; x++) {
+    randNum = arr[Math.floor(Math.random() * arr.length)]
+    result.push(randNum)
+    arr.splice(arr.indexOf(randNum), 1)
+  }
+  return result
 }
 
 
@@ -22,7 +34,7 @@ const assert = require('assert');
 describe('Sample', () => {
   it('behaves correctly when no second parameter is given', () => {
     assert.equal(sample([1]), 1);
-    assert.equal(sample([1,2]).length, 1);
+    assert.equal(sample([1, 2]).length, 1);
   });
   it('behaves correctly on negative n', () => {
     assert.equal(sample([1], -2), 1);
