2f7c36f45f3316122fee8786bdcece664a26e5aa
kieran-lockyer <kieran.lockyer.87@gmail.com>
2019-01-18 08:34:53 +1000

    sync master

--- 29_valueAndTimes.js
+++ 29_valueAndTimes.js
@@ -14,22 +14,32 @@ If your feeling confident add more tests below.
 */
 
 const valueAndTimes = (value, times) => {
-    // Your code here
+    // return times.toString().match(/[0-9]/) ? value.toString().match(/[a-z]/) ? value.repeat(times) : value * times : null
+
+    if (times.toString().match(/[0-9]/)) {
+        if (value.toString().match(/[a-z]/gi)) {
+            return value.repeat(times)
+        } else {
+            return value * times
+        }
+    } else {
+        return null
+    }
 }
 
 let assert = require('assert')
 
 describe('Multiply tests', () => {
     it('Should multiply the values and times when both are numbers', () => {
-        assert.equal(valueAndTimes(9,2), 18)
-        assert.equal(valueAndTimes(9.5,2.1), 19.95)
+        assert.equal(valueAndTimes(9, 2), 18)
+        assert.equal(valueAndTimes(9.5, 2.1), 19.95)
     })
     it('Should multiply when the value is a string', () => {
-        assert.equal(valueAndTimes("[alright]",3), "[alright][alright][alright]")
+        assert.equal(valueAndTimes("[alright]", 3), "[alright][alright][alright]")
     })
     it('Should return null when the times is not a number', () => {
-        assert.equal(valueAndTimes(4,"[kanye west]"), null)
-        assert.equal(valueAndTimes(2,"taylor swift"), null)
-        assert.equal(valueAndTimes(7, {a: "matthew mcconaughey"}), null)
+        assert.equal(valueAndTimes(4, "[kanye west]"), null)
+        assert.equal(valueAndTimes(2, "taylor swift"), null)
+        assert.equal(valueAndTimes(7, { a: "matthew mcconaughey" }), null)
     })
 })
