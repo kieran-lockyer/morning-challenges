fa779b947e2326984185bf0acf735a2e7231d8a0
kieran-lockyer <kieran.lockyer.87@gmail.com>
2019-01-21 09:27:57 +1000

    morning challenge completed

--- 36_map.js
+++ 36_map.js
@@ -21,8 +21,18 @@ Check you solutions using mocha 36_map.js.
 
 // Your code here
 
-const map = () => {
-
+const map = (arg, action) => {
+    result = []
+    if (Array.isArray(arg)) {
+        for (let i = 0; i < arg.length; i++) {
+            result.push(action(arg[i]))
+        }
+    } else {
+        for (let key of Object.keys(arg)) {
+            result.push(action(arg[key]))
+        }
+    }
+    return result
 }
 
 
@@ -30,11 +40,11 @@ let assert = require('assert')
 
 describe('Map', () => {
     it('can map arrays', () => {
-        const result = map([1,2,3,4], (num) => { return num * 3 })
-        assert.deepEqual(result, [3,6,9,12])
+        const result = map([1, 2, 3, 4], (num) => { return num * 3 })
+        assert.deepEqual(result, [3, 6, 9, 12])
     })
     it('can map objects', () => {
-        const result = map({one: 1, two: 2, three: 3}, (num) => { return num * 3 })
-        assert.deepEqual(result, [3,6,9])
+        const result = map({ one: 1, two: 2, three: 3 }, (num) => { return num * 3 })
+        assert.deepEqual(result, [3, 6, 9])
     })
 })
